6fd93c22ae4080c1e80cd94caddf296dbe88115a
justinkim2015 <justinkim2015@gmail.com>
2022-03-21 01:37:35 +0900

    Create grid

---
+++ main.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+
+
+
+
+for (let i=0; i < (256); i++) {
+  const box = document.createElement('div');
+  box.classList.add('box');
+  container.appendChild(box)
+}
