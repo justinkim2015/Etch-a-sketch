300a905105345958bba10158cc0ad008d4404421
justinkim2015 <justinkim2015@gmail.com>
2022-03-22 22:39:43 +0900

    Refactor finish button so that there is no initial prompt

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let gridSize = 24;
+let gridSize = 16;
 let percent = 100 / gridSize;
 let randomColor = Math.floor(Math.random()*16777215).toString(16);
 
@@ -12,18 +12,30 @@ const container = document.querySelector('.container');
 
 makeGrid(gridSize);
 
-
 resetButton.addEventListener('click', function() {
   gridSize = prompt('?');
-  box.forEach(box => box.style.backgroundColor = 'lightgrey');
+  console.log(gridSize);
+  DeleteGrid();
+  makeGrid(gridSize);
+  sizeGrid();
 });
 
+
+function DeleteGrid() { //this function is working as intended
+  let grid = document.querySelectorAll('.box');
+  for (grid of grid) {
+  container.removeChild(grid)
+  }
+}
+
+//deletegrid is deleting the element so theres no container to append to
+//this function is creating empty divs with the class box, it has the correct CSS applied but no padding or width so the grid doesnt show on screen.
 function makeGrid(size) {
 for (let i=0; i < (size * size); i++) {
     if (gridSize <= 100) {
-      const box = document.createElement('div');
-      box.classList.add('box');
-      container.appendChild(box);
+      const boxDiv = document.createElement('div');
+      boxDiv.classList.add('box');
+      container.appendChild(boxDiv);
     } else {
       alert("100 IS THE MAX");
       break;
@@ -31,14 +43,19 @@ for (let i=0; i < (size * size); i++) {
   }
 }
 
-const box = document.querySelectorAll('.box');
 
-box.forEach(box => box.style.width = `${percent}%`);
-box.forEach(box => box.style.paddingBottom = `${percent}%`);
+sizeGrid();
 
-box.forEach(box => box.addEventListener('mouseenter', function(){
-    box.style.backgroundColor = "#" + randomColor;
-}));
+function sizeGrid() {
+  let percent = 100 / gridSize;
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  const blockSize = document.querySelectorAll('.box');
+  blockSize.forEach(blockSize => blockSize.style.width = `${percent}%`);
+  blockSize.forEach(blockSize => blockSize.style.paddingBottom = `${percent}%`);
+  blockSize.forEach(blockSize => blockSize.addEventListener('mouseenter', function(){
+    blockSize.style.backgroundColor = "#" + randomColor;
+  }));
+}
 
 //This is the non-arrow function version of the above
 //box.forEach(function(box){
