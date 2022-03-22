2be2704533019fcfc5646e803f60ac24388e790b
justinkim2015 <justinkim2015@gmail.com>
2022-03-22 23:04:28 +0900

    Refactor functions for readability

--- main.js
+++ main.js
@@ -1,35 +1,25 @@
 let gridSize = 16;
 let percent = 100 / gridSize;
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 const resetButton = document.querySelector('.reset');
 const container = document.querySelector('.container');
-
-// resetButton.addEventListener('click', function(){
-//   gridSize = prompt('What\'s the new size bub?');
-//   console.log(gridSize);
-// });
-
-makeGrid(gridSize);
 
 resetButton.addEventListener('click', function() {
   gridSize = prompt('?');
   console.log(gridSize);
   DeleteGrid();
   makeGrid(gridSize);
-  sizeGrid();
+  styleGrid();
 });
 
 
-function DeleteGrid() { //this function is working as intended
+function DeleteGrid() {
   let grid = document.querySelectorAll('.box');
   for (grid of grid) {
   container.removeChild(grid)
   }
 }
 
-//deletegrid is deleting the element so theres no container to append to
-//this function is creating empty divs with the class box, it has the correct CSS applied but no padding or width so the grid doesnt show on screen.
 function makeGrid(size) {
 for (let i=0; i < (size * size); i++) {
     if (gridSize <= 100) {
@@ -43,27 +33,32 @@ for (let i=0; i < (size * size); i++) {
   }
 }
 
-
-sizeGrid();
-
-function sizeGrid() {
+function styleGrid() {
   let percent = 100 / gridSize;
   let randomColor = Math.floor(Math.random()*16777215).toString(16);
   const blockSize = document.querySelectorAll('.box');
   blockSize.forEach(blockSize => blockSize.style.width = `${percent}%`);
   blockSize.forEach(blockSize => blockSize.style.paddingBottom = `${percent}%`);
+  recolor();
+}
+
+function recolor() {
+  const blockSize = document.querySelectorAll('.box');
   blockSize.forEach(blockSize => blockSize.addEventListener('mouseenter', function(){
+    let randomColor = Math.floor(Math.random()*13773325).toString(16);
     blockSize.style.backgroundColor = "#" + randomColor;
   }));
 }
 
 //This is the non-arrow function version of the above
-//box.forEach(function(box){
-//    box.addEventListener('mouseenter', function(){
-//        box.style.backgroundColor = 'green';
+//box.forEach(function(blockSize){
+//    blockSize.addEventListener('mouseenter', function(){
+//        BlockSize.style.backgroundColor = 'green';
 //    })
 //})
 
+makeGrid(gridSize);
+styleGrid();
 
 
 
